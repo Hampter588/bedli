c49705b2c89a66a5bd98cab087206adae6110112
Hampter <bradbeeschwarz@gmail.com>
2026-09-22 17:53:19 -0700

    Clear BEDLI repo for handoff

--- Upstream/UpdateProcessor/Classes/DownloadLinkResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace BedrockLauncher.UpdateProcessor.Classes
-{
-    public struct DownloadLinkResult
-    {
-        public List<FileLocation> files;
-    };
-}
